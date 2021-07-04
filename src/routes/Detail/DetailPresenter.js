2b8cc644b668375c67b31433ae367b5b49f24849
giyun-cloud <nangiyoon@gmail.com>
2021-07-04 19:36:18 +0900

    6.8 Detail Container part One

--- src/routes/Detail/DetailPresenter.js
+++ src/routes/Detail/DetailPresenter.js
@@ -6,8 +6,13 @@ import Loader from "components/Loader";
 const Container = styled.div`
   height: calc(100vh - 50px);
   width: 100%;
+  position: relative;
+  padding: 50px;
 `;
 const Backdrop = styled.div`
+  top: 0;
+  left: 0;
+  position: absolute;
   height: 100%;
   width: 100%;
   background-image: url(${(prop) => prop.bgUrl});
@@ -15,11 +20,23 @@ const Backdrop = styled.div`
   background-position: center center;
   filter: blur(3px);
   opacity: 0.5;
+  z-index: 0;
 `;
 const Content = styled.div`
   display: flex;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+  height: 100%;
 `;
-const Cover = styled.div``;
+const Cover = styled.div`
+  width: 30%;
+  background-image: url(${(props) => props.bgUrl});
+  background-position: center center;
+  background-size: cover;
+  height: 100%;
+  border-radius: 5px;
+`;
 
 function DetailPresenter({ result, error, loading }) {
   return loading ? (
@@ -33,6 +50,15 @@ function DetailPresenter({ result, error, loading }) {
             : "https://upload.wikimedia.org/wikipedia/commons/6/6d/No_image.png"
         }
       />
+      <Content>
+        <Cover
+          bgUrl={
+            result.poster_path
+              ? `https://image.tmdb.org/t/p/w300${result.poster_path}`
+              : "https://upload.wikimedia.org/wikipedia/commons/6/6d/No_image.png"
+          }
+        />
+      </Content>
     </Container>
   );
 }
